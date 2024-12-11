7982621d05545eb6a38a0783099a6a5426e276fb
Kaian Muniz <kaianmuniz5@gmail.com>
2024-12-11 18:48:27 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -3,25 +3,25 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Habilita CORS para todas as requisições
+CORS(app) 
 
 
-# Função para se conectar ao banco de dados
+
 def get_db_connection():
     conn = sqlite3.connect('database/ongoticos.db')  
-    conn.row_factory = sqlite3.Row  # Para retornar os resultados como dicionários
+    conn.row_factory = sqlite3.Row  
     return conn
 
 
-# Rota para obter todos os animais
+
 @app.route('/api/animals', methods=['GET'])
 def get_animals():
     conn = get_db_connection()
-    animals = conn.execute('SELECT * FROM animals').fetchall()  # Consultar todos os animais
+    animals = conn.execute('SELECT * FROM animals').fetchall()  
     conn.close()
-    return jsonify([dict(animal) for animal in animals])  # Retorna os animais como uma lista de dicionários
+    return jsonify([dict(animal) for animal in animals])  
 
-# Rota para adicionar um novo animal
+
 @app.route('/api/animals', methods=['POST'])
 def add_animal():
     new_animal = request.get_json()
@@ -34,15 +34,15 @@ def add_animal():
     conn.close()
     return jsonify(new_animal), 201
 
-# Rota para obter todos os funcionários
+
 @app.route('/api/employees', methods=['GET'])
 def get_employees():
     conn = get_db_connection()
-    employees = conn.execute('SELECT * FROM employees').fetchall()  # Consultar todos os funcionários
+    employees = conn.execute('SELECT * FROM employees').fetchall()  
     conn.close()
-    return jsonify([dict(employee) for employee in employees])  # Retorna os funcionários como uma lista de dicionários
+    return jsonify([dict(employee) for employee in employees])  
 
-# Rota para adicionar um novo funcionário
+
 @app.route('/api/employees', methods=['POST'])
 def add_employee():
     new_employee = request.get_json()
@@ -55,7 +55,7 @@ def add_employee():
     conn.close()
     return jsonify(new_employee), 201
 
-# Rota para obter a história de um animal
+
 @app.route('/api/animal_history/<int:animal_id>', methods=['GET'])
 def get_animal_history(animal_id):
     conn = get_db_connection()
@@ -64,10 +64,10 @@ def get_animal_history(animal_id):
     ).fetchone()
     conn.close()
     if animal_history:
-        return jsonify(dict(animal_history))  # Retorna o histórico do animal
+        return jsonify(dict(animal_history))  
     else:
         return jsonify({"message": "History not found for this animal"}), 404
 
-# Rota para rodar o servidor
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
